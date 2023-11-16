0a52aaba43325d29917d016c437453d8124c75cf
SimonTian <835858151@qq.com>
2023-11-16 16:09:20 +0800

    creator spine preview

---
+++ cocos-creator/编辑器扩展/OpenDebugSpineInEditorTool.ts
@@ -0,0 +1,51 @@
+
+const {ccclass, property, menu} = cc._decorator;
+
+@ccclass
+@menu("编辑器组件/打开spine预览")
+export default class OpenDebugSpineInEditorTool extends cc.Component {
+
+    resetInEditor() {
+        if (!CC_EDITOR) return;
+
+        sp.Skeleton.prototype.update = function(dt) {
+            if (this.paused) return;
+
+            cc['engine']._animatingInEditMode = 1;
+
+            dt *= this.timeScale * sp.timeScale;
+    
+            if (this.isAnimationCached()) {
+    
+                // Cache mode and has animation queue.
+                if (this._isAniComplete) {
+                    if (this._animationQueue.length === 0 && !this._headAniInfo) {
+                        let frameCache = this._frameCache;
+                        if (frameCache && frameCache.isInvalid()) {
+                            frameCache.updateToFrame();
+                            let frames = frameCache.frames;
+                            this._curFrame = frames[frames.length - 1];
+                        }
+                        return;
+                    }
+                    if (!this._headAniInfo) {
+                        this._headAniInfo = this._animationQueue.shift();
+                    }
+                    this._accTime += dt;
+                    if (this._accTime > this._headAniInfo.delay) {
+                        let aniInfo = this._headAniInfo;
+                        this._headAniInfo = null;
+                        this.setAnimation (0, aniInfo.animationName, aniInfo.loop);
+                    }
+                    return;
+                }
+    
+                this._updateCache(dt);
+            } else {
+                this._updateRealtime(dt);
+            }
+        }
+
+        this.node.removeComponent(OpenDebugSpineInEditorTool);
+    }
+}
